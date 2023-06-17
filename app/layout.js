72880387f16c588a385c5bfea7b63b5e2d3f2ed7
Dominic Clerici <dclerici77@gmail.com>
2023-06-17 02:50:42 -0700

    pushing to google pt 2

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google"
 import localFont from "next/font/local"
 import Header from "@components/Universal/Header/Header"
 import styles from "./global.css"
+import Head from "next/head"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,6 +28,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${inter.variable} ${satoshi.variable}`}>
+      <Head>
+        <meta name="robots" content="all"></meta>
+        <meta name="googlebot" content="all"></meta>
+        <meta
+          name="google-site-verification"
+          content="fft1nfHhHvN5tBCg6G3d5G2W6OzJYmhZVq7HXpblvM8"
+        ></meta>
+      </Head>
       <body>
         <Header></Header>
         <main>{children}</main>
