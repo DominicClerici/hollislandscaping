797e2a807948a6a43a69160c1ff99ced77de8579
Dominic Clerici <dclerici77@gmail.com>
2023-06-17 02:55:31 -0700

    pt 3

--- app/layout.js
+++ app/layout.js
@@ -15,20 +15,25 @@ const satoshi = localFont({
   variable: "--font-satoshi",
 })
 
-export const metadata = {
-  title: "Hollis Landscaping - Commercial & Residential",
-  description:
-    "The Best Professional Landscape Maintenance And Design Services For Residential and Commercial Properties",
-  robots: "all",
-  googlebot: "all",
-  google: "nositelinkssearchbox",
-  "google-site-verification": "fft1nfHhHvN5tBCg6G3d5G2W6OzJYmhZVq7HXpblvM8",
-}
+// export const metadata = {
+//   title: "Hollis Landscaping - Commercial & Residential",
+//   description:
+//     "The Best Professional Landscape Maintenance And Design Services For Residential and Commercial Properties",
+// }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${inter.variable} ${satoshi.variable}`}>
       <Head>
+        <title>Hollis Landscaping - Commercial & Residential</title>
+        <meta
+          name="title"
+          content="Hollis Landscaping - Commercial & Residential"
+        ></meta>
+        <meta
+          name="description"
+          content="The Best Professional Landscape Maintenance And Design Services For Residential and Commercial Properties"
+        ></meta>
         <meta name="robots" content="all"></meta>
         <meta name="googlebot" content="all"></meta>
         <meta
